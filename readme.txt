7f06365518e45869262437cfd4f6fa41fccf60e2
lib <lib@servyou.com.cn>
2019-05-06 13:05:24 +0800

    增加spring boot对redis缓存整合demo

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 1.使用spring initializr初始化的maven项目，同时集成了mybatis配置，有一个简单的demo（后期spring boot项目可以直接使用这个空白项目进行开发）
-2.增加了定时器的demo案例
+2.增加了过滤器的demo
+3.增加了定时器的demo
+4.增加了缓存使用的demo
--- src/test/java/com/shuiyou/myspringboot/MyspringbootApplicationTests.java
+++ src/test/java/com/shuiyou/myspringboot/MyspringbootApplicationTests.java
@@ -1,16 +1,50 @@
 package com.shuiyou.myspringboot;
 
+import com.shuiyou.myspringboot.entity.User;
+import com.shuiyou.myspringboot.util.RedisUtil;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyspringbootApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private RedisUtil redisUtil;
+
     @Test
     public void contextLoads() {
     }
+
+    // 测试直接使用redis
+    @Test
+    public void testStringWithRedis(){
+        stringRedisTemplate.opsForValue().set("name", "guanguan");
+        String val = stringRedisTemplate.opsForValue().get("name");
+        Assert.assertEquals("guanguan", val);
+    }
+    // 测试使用RedisUtil工具类分别存入字符串和序列化的对象
+    @Test
+    public void testRedisUtilSetString(){
+        // 缓存60秒
+        redisUtil.set("bingoString", 222,60L);
+    }
+    @Test
+    public void testRedisUtilSetObject(){
+        User user = new User();
+        user.setAge(18);
+        user.setId(1);
+        user.setPassword("123456");
+        user.setUserName("bingoObject");
+        redisUtil.set("bingoObject",user);
+    }
+
 
 }
